619524e74267932c51aa65f37dabad4120028d1c
Frederick Javalera <frederickjavalera@gmail.com>
2020-05-10 10:53:17 -0600

    Added more examples.

--- IOPracticeWithCSharp/Program.cs
+++ IOPracticeWithCSharp/Program.cs
@@ -8,7 +8,7 @@ namespace IOPracticeWithCSharp
         static void Main(string[] args)
         {
             /*
-             * Using File type static method.
+             * 1. Using File type static method.
              */
 
             var testPath = "/Users/frederickjavalera/Downloads/test.txt";
@@ -62,7 +62,7 @@ namespace IOPracticeWithCSharp
             }
 
             /*
-             * Using FileInfo type method.
+             * 2. Using FileInfo type method.
              */
 
             var fileInfo = new FileInfo(testPath);
@@ -82,6 +82,81 @@ namespace IOPracticeWithCSharp
 
             // Read all of the text in the file and write to console.
             // Uses FileStream. Lesson forthcoming.
+
+            /**
+             ** Working with directories.
+             **/
+
+            /*
+             * 1. Using Directory static method.
+             */
+
+            var tempDirectoryPath = "/Users/frederickjavalera/Downloads/tempDirectoryFolder";
+            var containerDirectory = "/Users/frederickjavalera/Downloads/";
+
+            // Create a new directory.
+            Directory.CreateDirectory(tempDirectoryPath);
+
+            // Find all pdf files in Downloads folder. For all files use "." instead of "*.pdf".
+            var files = Directory.GetFiles(containerDirectory, "*.pdf", SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            // Find all directories within a directory.
+            var directories = Directory.GetDirectories(containerDirectory, "*.*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+
+            // Check if directory exists.
+            if (Directory.Exists(containerDirectory))
+            {
+                // Do something.
+            }
+
+            /*
+             * Using DirectoryInfo type method.
+             */
+            var directoryInfo = new DirectoryInfo(containerDirectory);
+
+            // Retrieves all files in the directory.
+            var files2 = directoryInfo.GetFiles();
+            foreach (var file in files2)
+            {
+                Console.WriteLine(file);
+            }
+
+            // Retrieves all directories with the directory.
+            var directories2 = directoryInfo.GetDirectories();
+            foreach (var directory in directories2)
+            {
+                Console.WriteLine(directory);
+            }
+
+            /**
+             ** Working with Paths.
+             **/
+            var path = "/Users/frederickjavalera/Downloads/test.txt";
+
+            // Bad way of getting extension of file.
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+            Console.WriteLine($"Extension, bad way: {extension}");
+
+            // Better way using Path class.
+            Console.WriteLine($"Extension, better way: {Path.GetExtension(path)}");
+
+            // Get file name.
+            Console.WriteLine($"File name: {Path.GetFileName(path)}");
+
+            // Get file name without extension.
+            Console.WriteLine($"File name without extension: {Path.GetFileNameWithoutExtension(path)}");
+
+            // Get directory name.
+            Console.WriteLine($"Directory name: {Path.GetDirectoryName(path)}");
         }
     }
 }
